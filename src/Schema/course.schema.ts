43e3446e051e122259e1fc56cf37ff199e08edf3
Rafael <rgarrido846@gmail.com>
2023-08-18 14:43:27 -0300

    feat: create course schema

---
+++ src/Schema/course.schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+const courseSchema = z.object({
+  id: z.number().positive(),
+  name: z.string().max(15).nonempty({ message: "Required" }),
+  description: z
+    .string()
+    .nonempty({ message: "Expected string, received number" }),
+});
+
+const courseCreateSchema = courseSchema.omit({ id: true });
+const courseReadSchema = courseSchema.array();
+
+export { courseSchema, courseCreateSchema, courseReadSchema };
